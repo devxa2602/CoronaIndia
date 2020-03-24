249d5e75fe3d17985fbab7676f513a7dc76b6fd2
github_devxa2602 <akshayrawate@gmail.com>
2020-03-24 20:38:56 +0530

    Updates

---
+++ precache-manifest.53a645f521c5f777b56b2df8bdd02a58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "060f9c17d2f2ff36080ac4ca2dea8a62",
+    "url": "/CoronaIndia/index.html"
+  },
+  {
+    "revision": "10124f9bfb4c7a05c558",
+    "url": "/CoronaIndia/static/css/main.892daef6.chunk.css"
+  },
+  {
+    "revision": "34970207cf9310efb1a9",
+    "url": "/CoronaIndia/static/js/2.e35c06d4.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/CoronaIndia/static/js/2.e35c06d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10124f9bfb4c7a05c558",
+    "url": "/CoronaIndia/static/js/main.80259297.chunk.js"
+  },
+  {
+    "revision": "a2726149102eaa7cb10f",
+    "url": "/CoronaIndia/static/js/runtime-main.8ba3427b.js"
+  }
+]);
